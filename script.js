a8a7b4ed939aaf15dcdf464533fd520104731f52
ajajff10-hub <ajaj.ff10@gmail.com>
2026-07-01 20:05:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 === 0) {
-        return "ERROR: Nice try!";
+        return "Nice try!";
     }
     return num1 / num2;
 }
