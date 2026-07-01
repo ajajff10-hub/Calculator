dd1945b4ff59d4c43aa1ac8ad1638d37e03f0ee6
ajajff10-hub <ajaj.ff10@gmail.com>
2026-07-01 16:02:56 +0300

    4 operations functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "ERROR: Nice try!";
+    }
+    return num1 / num2;
+}
