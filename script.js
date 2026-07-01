7ad038aabba362b47fc3ce8c9a040d73b748a2d9
ajajff10-hub <ajaj.ff10@gmail.com>
2026-07-01 16:05:52 +0300

    operate function

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function divide(num1, num2) {
     }
     return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Invalid Operator";
+    }
+}
